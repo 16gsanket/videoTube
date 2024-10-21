91166c368f2490b2f841bab8f57ba24bd2658537
16gsanket <16gsanket@gmail.com>
2024-10-21 19:04:26 +0530

    code added for database connection ---> error for connectiong IP whitelisting

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async ()=>{
+    try {
+        console.log("attemtping to connect DB to ", process.env.MONGO_URI , DB_NAME);
+        
+        const connectionInstances = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        console.log('db connected');
+        
+        console.log(`MongoDB Connected: ${connectionInstances.connection.host}`)
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+ 
+        process.exit(1);
+      }
+}
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('started the server');
+import { app } from "./app.js";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config(
+    {
+        path:"./.env"
+    }
+)
+
+const PORT = 3000 || process.env.PORT;
+
+connectDB().then(()=>{
+     app.listen(PORT, ()=>{
+        console.log(`Server running on port ${PORT}`)
+    })
+}).catch((error)=>{
+    console.log('mongoDB connection error',error)
+})
+
+
+

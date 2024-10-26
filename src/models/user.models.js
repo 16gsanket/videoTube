3e12a53205a1d8c98ae4476984c188ec52797ebf
16gsanket <16gsanket@gmail.com>
2024-10-26 19:35:12 +0530

    added refersh token and access token

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose , {Schema} from "mongoose";
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
@@ -46,6 +47,42 @@ const userSchema = new Schema(
         }
     },{timestamps:true}
 )
+
+
+// this is used for saving the password in the encrypter format
+userSchema.pre("save" , async function(next){
+
+
+    if(!this.modified("password")) return next();
+
+    this.password = bcrypt.hash(this.password,10);
+    next()
+})
+
+// following method tp check the password and is to be mointed on the userSchema directly
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password , this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    var token = jwt.sign(
+        { 
+          id = this._id,
+          email = this.email
+         },
+         process.env.ACCESS_TOKEN_SECRET, 
+         { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+        );
+}
+userSchema.methods.generateRefreshToken = function(){
+    var token = jwt.sign(
+        { 
+          id = this._id,
+         },
+         process.env.REFRESH_TOKEN_SECRET, 
+         { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+        );
+}
 
 
 // this creates a document User(users) with reference to the schema userSchema
